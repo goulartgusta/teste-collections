46cfb048d4dbef269f37fdbb4a0494340b910803
Gustavo Goulart <goulartgusta9@gmail.com>
2024-12-23 11:34:20 -0300

    test: comportamentos de List

--- src/test/java/br/com/almaviva/teste/collection/comportamentos/ListComportamentoTeste.java
+++ src/test/java/br/com/almaviva/teste/collection/comportamentos/ListComportamentoTeste.java
@@ -1,5 +1,77 @@
 package br.com.almaviva.teste.collection.comportamentos;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import br.com.almaviva.teste.collection.comportamentos.models.ListDados;
+
 public class ListComportamentoTeste {
+
+	static Stream<List<Character>> utilizarListas() {
+		return Stream.of(new ArrayList<>(), new LinkedList<>(), new CopyOnWriteArrayList<>());
+	}
+
+	@ParameterizedTest
+	@MethodSource("utilizarListas")
+	void deveriaAdicionarElementosNaLista(List<Character> list) {
+		list.add(ListDados.LETRA_A.getLetra());
+		list.add(ListDados.LETRA_B.getLetra());
+		list.add(ListDados.LETRA_C.getLetra());
+
+		assertEquals(3, list.size());
+		assertEquals(ListDados.LETRA_A.getLetra(), list.get(0));
+		assertEquals(ListDados.LETRA_B.getLetra(), list.get(1));
+		assertEquals(ListDados.LETRA_C.getLetra(), list.get(2));
+	}
+
+	@ParameterizedTest
+	@MethodSource("utilizarListas")
+	void deveriaRemoverElementoPeloIndice(List<Character> list) {
+		list.add(ListDados.LETRA_A.getLetra());
+		list.add(ListDados.LETRA_B.getLetra());
+		list.add(ListDados.LETRA_C.getLetra());
+
+		list.remove(1); 
+
+		assertEquals(2, list.size());
+		assertFalse(list.contains(ListDados.LETRA_B.getLetra()));
+		assertEquals(ListDados.LETRA_C.getLetra(), list.get(1));
+	}
+
+	@ParameterizedTest
+	@MethodSource("utilizarListas")
+	void deveriaInserirElementoEmPosicaoEspecifica(List<Character> list) {
+		list.add(ListDados.LETRA_A.getLetra());
+		list.add(ListDados.LETRA_C.getLetra());
+
+		list.add(1, ListDados.LETRA_B.getLetra());
+
+		assertEquals(3, list.size());
+		assertEquals(ListDados.LETRA_B.getLetra(), list.get(1));
+		assertEquals(ListDados.LETRA_C.getLetra(), list.get(2));
+	}
+
+	@ParameterizedTest
+	@MethodSource("utilizarListas")
+	void deveriaSubstituirElementoEmPosicaoEspecifica(List<Character> list) {
+		list.add(ListDados.LETRA_A.getLetra());
+		list.add(ListDados.LETRA_B.getLetra());
+
+		list.set(1, ListDados.LETRA_C.getLetra()); 
+
+		assertEquals(2, list.size());
+		assertEquals(ListDados.LETRA_C.getLetra(), list.get(1));
+		assertFalse(list.contains(ListDados.LETRA_B.getLetra()));
+	}
+
 
 }
--- src/test/java/br/com/almaviva/teste/collection/comportamentos/models/ListDados.java
+++ src/test/java/br/com/almaviva/teste/collection/comportamentos/models/ListDados.java
@@ -1,5 +1,27 @@
 package br.com.almaviva.teste.collection.comportamentos.models;
 
 public enum ListDados {
-  
+    LETRA_A('A'), LETRA_B('B'), LETRA_C('C'),
+    TAMANHO_ZERO(0), TAMANHO_PADRAO(2);
+
+    private final Character letra;
+    private final Integer tamanho;
+
+    ListDados(char letra) {
+        this.letra = letra;
+        this.tamanho = null;
+    }
+
+    ListDados(int tamanho) {
+        this.letra = null;
+        this.tamanho = tamanho;
+    }
+
+    public Character getLetra() {
+        return letra;
+    }
+
+    public Integer getTamanho() {
+        return tamanho;
+    }
 }
